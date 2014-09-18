be2d4829ddad87984a2f9a8436444b4dee4472cd
lamd <lamd@dbce743a-9276-4de6-8aa5-79eb1587b567>
2014-09-18 21:13:17 +0000

    Added missing class.
    git-svn-id: https://10.10.10.25/ode@97 dbce743a-9276-4de6-8aa5-79eb1587b567

---
+++ trunk/ode-api/src/main/java/com/leidos/ode/agent/SubscribeODEAgent.java
@@ -0,0 +1,18 @@
+package com.leidos.ode.agent;
+
+import com.leidos.ode.core.data.ODERegistrationResponse;
+
+/**
+ * This controller is used when a collector will only subscribe to data feeds.
+ *
+ * @author cassadyja
+ */
+public class SubscribeODEAgent extends ODEAgent {
+
+    @Override
+    public void startUp() {
+        ODERegistrationResponse regResponse = registration.register(registrationInformation);
+        createAgentInfo(regResponse);
+    }
+
+}
