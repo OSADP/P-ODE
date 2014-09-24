2be399783142daa553ac3fd87a6fbc08356b18c1
lamd <lamd@dbce743a-9276-4de6-8aa5-79eb1587b567>
2014-09-24 20:05:08 +0000

    Added separated ode-emulator-web module and ode-emulator. Configuration changes for web and Spring.
    git-svn-id: https://10.10.10.25/ode@106 dbce743a-9276-4de6-8aa5-79eb1587b567

--- trunk/ode-emulator/src/main/java/com/leidos/ode/emulator/ODEEmulator.java
+++ trunk/ode-emulator/src/main/java/com/leidos/ode/emulator/ODEEmulator.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.leidos.ode.emulator;
 
 import com.leidos.ode.agent.data.ODEAgentMessage;
