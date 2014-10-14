0696ac47b54fd54be9ded9c2a0bb997b8b254f78
lamd <lamd@dbce743a-9276-4de6-8aa5-79eb1587b567>
2014-10-14 15:05:49 +0000

    Added missing files.
    git-svn-id: https://10.10.10.25/ode@156 dbce743a-9276-4de6-8aa5-79eb1587b567

---
+++ trunk/ode-api/src/main/java/com/leidos/ode/agent/data/ritis/RITISWeatherDataClarus.java
@@ -0,0 +1,23 @@
+package com.leidos.ode.agent.data.ritis;
+
+import edu.umd.cattlab.schema.ritisFilter.other.ClarusData;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: LAMDE
+ * Date: 10/10/14
+ * Time: 6:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RITISWeatherDataClarus {
+
+    private ClarusData clarusData;
+
+    public ClarusData getClarusData() {
+        return clarusData;
+    }
+
+    public void setClarusData(ClarusData clarusData) {
+        this.clarusData = clarusData;
+    }
+}
---
+++ trunk/ode-api/src/main/java/com/leidos/ode/agent/data/ritis/RITISWeatherDataNWS.java
@@ -0,0 +1,23 @@
+package com.leidos.ode.agent.data.ritis;
+
+import edu.umd.cattlab.schema.ritisFilter.other.AlertsData;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: LAMDE
+ * Date: 10/8/14
+ * Time: 11:15 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RITISWeatherDataNWS {
+
+    private AlertsData alertsData;
+
+    public AlertsData getAlertsData() {
+        return alertsData;
+    }
+
+    public void setAlertsData(AlertsData alertsData) {
+        this.alertsData = alertsData;
+    }
+}
